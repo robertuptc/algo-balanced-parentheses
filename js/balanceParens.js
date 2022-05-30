21252bc738f35cab1128e2b3f9c1e1dfd73e9685
robertuptc <robertuptc@gmail.com>
2022-05-30 12:08:49 -0400

    finished exercises

--- js/balanceParens.js
+++ js/balanceParens.js
@@ -1,5 +1,23 @@
-balanceParens = (str) =>
-{
+balanceParens = (str) => {
+    let strSplit = str.split('');
+    let stack = [];
+
+    for (let i = 0; i < strSplit.length; i++) {
+        if (strSplit[i] === ')') {
+            if (stack.length !== 0) {
+                stack.pop()
+            } else {
+                strSplit[i] = ''
+            }
+        } else if (strSplit[i] === '(') {
+            stack.push(i)
+        }
+    } 
+
+    for (let i = 0; i < stack.length; i++) {
+        strSplit[stack[i]] = ''
+    }
+    return strSplit.join('')
 
 }
 
--- python/balance_parens.py
+++ python/balance_parens.py
@@ -1,2 +1,17 @@
 def balance_parens(str):
-    pass
+    split_str = list(str)
+    stack = []
+
+    for idx, char in enumerate(split_str) :
+        if char == ")" :
+            if stack:
+                stack.pop()
+            else:
+                split_str[idx] = ''
+        elif char == "(" :
+            stack.append(idx)
+    
+    for i in stack :
+        split_str[i] = ''
+
+    return ''.join(split_str)
